2239c1ddc7e511a349a8fc90107c89847a23f444
Jym-lab <jym9809@gmail.com>
2023-07-30 18:48:37 +0900

    person.js print fix

--- reader/static/js/show-person.js
+++ reader/static/js/show-person.js
@@ -1,4 +1,4 @@
-fetch('js/person.json').then(function(response){
+fetch('/static/js/person.json').then(function(response){
     if(!response.ok){
         throw new Error('에러');
     }
@@ -8,7 +8,7 @@ fetch('js/person.json').then(function(response){
         const template = `
             <div class="col-6 col-md-4 col-lg-2 p-2">
                 <div class="profile-card text-center py-4 ${items.class}">
-                    <img class="img-fluid" src="${items.image}" alt="프로필 이미지">
+                    <img class="img-fluid" src="/static/${items.image}" alt="프로필 이미지">
                     <div class="pretendard-font maker-name text-white">${items.name}</div>
                     <div class="pretendard-font maker-department text-white">${items.department}</div>
                     <div class="pretendard-font maker-position mt-3 text-white">${items.position}</div>
